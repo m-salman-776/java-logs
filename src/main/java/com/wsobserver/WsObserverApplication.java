b4ae7ac724a7335d77316f0906df296ce707a98b
Salman <mohd.salman@nurture.farm>
2023-02-15 13:50:08 +0530

    git init

---
+++ src/main/java/com/wsobserver/WsObserverApplication.java
@@ -0,0 +1,12 @@
+package com.wsobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsObserverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wsobserver/WsObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
