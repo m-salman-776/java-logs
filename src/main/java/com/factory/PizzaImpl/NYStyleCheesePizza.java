ed216ece0a7efc7ca110311d6cc39a61c4e56772
Salman <mohd.salman@nurture.farm>
2023-03-06 21:41:51 +0530

    abstract factory patter

--- src/main/java/com/factory/PizzaImpl/NYStyleCheesePizza.java
+++ src/main/java/com/factory/PizzaImpl/NYStyleCheesePizza.java
@@ -11,6 +11,12 @@ public class NYStyleCheesePizza extends Pizza {
 //        this.toppings.add("Topping 2");
 //        this.toppings.add("Topping 3");
     }
+
+    @Override
+    protected void prepare() {
+
+    }
+
     @Override
     public void cut(){
         System.out.println("Cutting the pizza in NY Style");
